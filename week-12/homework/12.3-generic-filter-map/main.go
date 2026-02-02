686588cd8de837772a1cb8f77622985537aab642
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-02-02 15:14:37 +0500

    week-12: выполнено задание 12.3-generic-filter-map

--- week-12/homework/12.3-generic-filter-map/main.go
+++ week-12/homework/12.3-generic-filter-map/main.go
@@ -9,7 +9,15 @@ func Filter[T any](slice []T, predicate func(T) bool) []T {
 	// 2. Пройди по всем элементам
 	// 3. Если predicate(element) == true, добавь в результат
 	// 4. Верни результат
-	return nil
+	var res []T
+
+	for _, v := range slice {
+		if predicate(v) {
+			res = append(res, v)
+		}
+	}
+
+	return res
 }
 
 // Map преобразует каждый элемент слайса с помощью функции transform
@@ -19,7 +27,14 @@ func Map[T, R any](slice []T, transform func(T) R) []R {
 	// 2. Пройди по всем элементам
 	// 3. Преобразуй каждый элемент: result[i] = transform(slice[i])
 	// 4. Верни результат
-	return nil
+
+	res := make([]R, len(slice))
+
+	for i := range slice {
+		res[i] = transform(slice[i])
+	}
+
+	return res
 }
 
 // Reduce сворачивает слайс в одно значение
@@ -29,7 +44,14 @@ func Reduce[T, R any](slice []T, initial R, reducer func(R, T) R) R {
 	// 2. Пройди по всем элементам
 	// 3. Обнови аккумулятор: acc = reducer(acc, element)
 	// 4. Верни аккумулятор
-	return initial
+
+	acc := initial
+
+	for _, v := range slice {
+		acc = reducer(acc, v)
+	}
+
+	return acc
 }
 
 // Find ищет первый элемент, для которого predicate вернул true
@@ -39,6 +61,13 @@ func Find[T any](slice []T, predicate func(T) bool) (T, bool) {
 	// 2. Если predicate(element) == true, верни (element, true)
 	// 3. Если ничего не найдено, верни (zero, false)
 	var zero T
+
+	for _, v := range slice {
+		if predicate(v) {
+			return v, true
+		}
+	}
+
 	return zero, false
 }
 
@@ -46,6 +75,13 @@ func Find[T any](slice []T, predicate func(T) bool) (T, bool) {
 func Any[T any](slice []T, predicate func(T) bool) bool {
 	// TODO: реализуй функцию
 	// Верни true при первом совпадении
+
+	for _, v := range slice {
+		if predicate(v) {
+			return true
+		}
+	}
+
 	return false
 }
 
@@ -54,6 +90,17 @@ func Any[T any](slice []T, predicate func(T) bool) bool {
 func All[T any](slice []T, predicate func(T) bool) bool {
 	// TODO: реализуй функцию
 	// Верни false при первом несовпадении
+
+	if len(slice) == 0 {
+		return true
+	}
+
+	for _, v := range slice {
+		if !predicate(v) {
+			return false
+		}
+	}
+
 	return true
 }
 
