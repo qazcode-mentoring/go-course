ac337c5a93a51804a2faf542304eaf98d381acc6
fek1ss <damirfrontend.dev@gmail.com>
2026-02-04 12:00:14 +0600

    week-01: выполнены задания week-01

--- week-01/homework/1.2-swap/main.go
+++ week-01/homework/1.2-swap/main.go
@@ -5,7 +5,7 @@ import "fmt"
 // swap принимает два целых числа и возвращает их в обратном порядке
 func swap(a, b int) (int, int) {
 	// TODO: реализуй функцию
-	return 0, 0
+	return a, b
 }
 
 func main() {
