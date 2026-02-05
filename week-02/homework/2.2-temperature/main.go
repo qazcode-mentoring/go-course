3ced7039bb4c282ba60a9b059be3974baff21925
fek1ss <damirfrontend.dev@gmail.com>
2026-02-05 20:06:11 +0600

    week-02: выполнены задания 2 недели

--- week-02/homework/2.2-temperature/main.go
+++ week-02/homework/2.2-temperature/main.go
@@ -12,29 +12,42 @@ type Fahrenheit float64
 func CelsiusToFahrenheit(c Celsius) Fahrenheit {
 	// TODO: реализуй функцию
 	// Формула: F = C × 9/5 + 32
-	return 0
+	f := Fahrenheit((c * 9.0 / 5.0) + 32.0)
+	return f
 }
 
 // FahrenheitToCelsius конвертирует Фаренгейт в Цельсий
 func FahrenheitToCelsius(f Fahrenheit) Celsius {
 	// TODO: реализуй функцию
 	// Формула: C = (F - 32) × 5/9
-	return 0
+	c := Celsius((f - 32.0) * (5.0 / 9.0))
+	return c
 }
 
 func main() {
 	// Тест 1: 100°C = 212°F
-	c := Celsius(100)
+	c := Celsius(100.0)
 	f := CelsiusToFahrenheit(c)
 	fmt.Printf("%.1f°C = %.1f°F\n", c, f)
 
 	// Тест 2: 32°F = 0°C
-	f2 := Fahrenheit(32)
+	f2 := Fahrenheit(32.0)
 	c2 := FahrenheitToCelsius(f2)
 	fmt.Printf("%.1f°F = %.1f°C\n", f2, c2)
 
 	// TODO: Добавь проверку остальных тестовых значений:
 	// 0°C = 32°F
+	c1 := Celsius(0.0)
+	f1 := CelsiusToFahrenheit(c1)
+	fmt.Printf("%.1f°C = %.1f°F\n", c1, f1)
+
 	// -40°C = -40°F
+	c3 := Celsius(-40.0)
+	f3 := CelsiusToFahrenheit(c3)
+	fmt.Printf("%.1f°C = %1.f°F\n", c3, f3)
+
 	// 37°C = 98.6°F
+	c4 := Celsius(37.0)
+	f4 := CelsiusToFahrenheit(c4)
+	fmt.Printf("%.1f°C = %.1f°F\n", c4, f4)
 }
--- week-02/homework/2.3-weekdays/main.go
+++ week-02/homework/2.3-weekdays/main.go
@@ -8,12 +8,20 @@ const (
 	Monday = iota + 1
 	Tuesday
 	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
 	// TODO: добавь остальные дни
 )
 
 // IsWeekend возвращает true, если день является выходным (суббота или воскресенье)
 func IsWeekend(day int) bool {
 	// TODO: реализуй функцию
+	if day == 7 || day == 6 {
+		return true
+	}
+
 	return false
 }
 
@@ -21,10 +29,14 @@ func main() {
 	fmt.Println("Понедельник:", Monday)
 	fmt.Println("Вторник:", Tuesday)
 	fmt.Println("Среда:", Wednesday)
-	// TODO: выведи остальные дни
+	fmt.Println("Четверг:", Thursday)
+	fmt.Println("Пятница:", Friday)
+	fmt.Println("Суббота:", Saturday)
+	fmt.Println("Воскресенье:", Sunday)
 
 	fmt.Println()
 	fmt.Println("Понедельник - выходной?", IsWeekend(Monday))
 	fmt.Println("Среда - выходной?", IsWeekend(Wednesday))
-	// TODO: проверь субботу и воскресенье
+	fmt.Println("Суббота - выходной?", IsWeekend(Saturday))
+	fmt.Println("Воскресенье - выходной?", IsWeekend(Sunday))
 }
